1343794da07ff1db2b007236d70025f345ecce1b
Andrew Regan <aregan@gmail.com>
2016-05-28 23:13:48 +0100

    Reduce debug

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ console.log('# Loaded')
 bot.handler()
 console.log('# Started OK')
 bot.inst.postMessageToChannel('general', 'walk the talk');
-bot.inst.postMessageToChannel('general', 'breathtaking arrogance @ ' + new Date().toISOString());
-bot.inst.postMessageToChannel('general', 'many studies have shown @ ' + new Date().toISOString());
+bot.inst.postMessageToChannel('general', 'breathtaking arrogance');
+bot.inst.postMessageToChannel('general', 'many studies have shown');
 console.log('# Sent our own message')
